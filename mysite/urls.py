eb1ee4a8fa82d8fd7dd4bc179fda48f5a43caf73
Ірина Заєць <irynazaiets@MacBook-Air-Iryna.local>
2025-02-15 13:32:11 +0200

    Fixed a redirect to the logout page

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('register/', polls_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', 
+        next_page='login', http_method_names=['get', 'post']), name='logout'),
 ]
